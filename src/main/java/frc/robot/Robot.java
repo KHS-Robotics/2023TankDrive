333bcd5888a16ee2505f927ae6f415a21e03cca1
Ernie3 <ernestgwilsoniii@gmail.com>
2025-01-13 22:24:49 -0500

    update to 2025

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
@@ -19,22 +18,15 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private RobotContainer robotContainer;
-  private Command autonomousCommand;
 
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
+  public Robot() {
     // for debugging
     CommandScheduler.getInstance()
-      .onCommandInitialize((command) -> System.out.println(command.getName() + " started."));
+        .onCommandInitialize((command) -> System.out.println(command.getName() + " started."));
     CommandScheduler.getInstance()
-      .onCommandInterrupt((command) -> System.out.println(command.getName() + " interrupted."));
+        .onCommandInterrupt((command) -> System.out.println(command.getName() + " interrupted."));
     CommandScheduler.getInstance()
-      .onCommandFinish((command) -> System.out.println(command.getName() + " ended."));
+        .onCommandFinish((command) -> System.out.println(command.getName() + " ended."));
 
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
@@ -89,13 +81,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
-    }
   }
 
   /** This function is called periodically during operator control. */
